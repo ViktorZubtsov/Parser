38ce9606d598201c4e7834e795c8fb2f1ecbd3cf
mr.viktor.1996 <mr.viktor.1996@yandex.ru>
2020-08-02 23:26:50 +0700

    Write a Parser for the signin.json file.

--- js/parser.js
+++ js/parser.js
@@ -1,40 +1,142 @@
-function doSomething() {
+let data = {
+    "name":"register",
+    "fields":[
+    {
+        "input":{
+            "type":"text",
+            "required":true,
+            "placeholder":"Enter full name"
+        }
+    },
+    {
+        "input":{
+            "type":"email",
+            "required":true,
+            "placeholder":"Enter email"
+        }
+    },
+    {
+        "input":{
+            "type":"password",
+            "required":true,
+            "placeholder":"password"
+        }
+    },
+    {
+        "input":{
+            "type":"password",
+            "required":true,
+            "placeholder":"Confirm password"
+        }
+    }
+    ],
+    "references":[
+        {
+            "text without ref":"Already have account?",
+            "text":"Login",
+            "ref":"signin"
+        }
+    ],
+    "buttons":[
+    {
+        "text":"Sign Up"
+    }
+]
+};
 
-    let file = document.getElementById('file');
-    let reader = new FileReader();
 
-    if(file.files.length){
+
+
+
+let app = document.getElementById('app');
+let i = '';
+let elem = '';
+// parseSignup(data,app);
+
+
+function parseSignup(data,app) {
+
+
+    
+    app.innerHTML = `
+        <form action="" method="post" >
+            <legend>${data.name}</legend>
+            <fieldset class="fields">
+                ${renderInput(data)}
+            </fieldset>
+            <fieldset  class="send">
+                ${renderButtons(data)}
+                ${renderReferences(data)}
+            </fieldset>
+
+        </form> 
+    `;
+   
+
+    function renderInput(data) {
+        let elem = '';
+    
+        for (i in data.fields) {
+ 
+ 
+           elem += `<input type="${data.fields[i].input.type}"  
+                        placeholder="${data.fields[i].input.placeholder}" 
+                        ${data.fields[i].input.required == true ? 'required': ''}>`;
+
+
+        } 
+
+        return elem;
+    }
+
+    function renderButtons(data) {
+
         
-        reader.onload = function(e){
+        for (i in data.buttons) {
+     
 
-            let data = JSON.parse(reader.result);
-            console.log(data);
-
-                // Signin
-            let form = document.getElementById('form');
-            let name = data.name;
-            let fieldsAll = data.fields;
-            // let required;
-            // data.fields[0].input.required == true ? required = "required" : " ";
-
-            form.innerHTML+= '<p> ' + name + '<input type="text" size="40"> </p>';
-            form.innerHTML+=   '<label for="email">' + fieldsAll[0].label +'</label>';
-            form.innerHTML+= `<input placeholder=${data.fields[0].input.placeholder} type=${data.fields[0].input.type} />`;
-            form.innerHTML+=   '<label for="email">' + fieldsAll[1].label +'</label>';
-            form.innerHTML+= `<input  placeholder=${data.fields[1].input.placeholder} type=${data.fields[1].input.type} />`;
-
-            
+            elem += ` <button>${data.buttons[i].text}</button>`;
+  
+        } 
         
-
-        };
-        reader.readAsBinaryString(file.files[0]);
+        return elem;
         
     }
+
+    function renderReferences(data) {
+        elem = '';
+        for (i in data.references) {
+
+            elem += ` <span>${data.references[i]['text without ref']}</span><a href="${data.references[i].ref}">${data.references[i].text}</a>`;
+  
+        } 
+        
+        return elem;
+        
+    }
+  
 }
 
 
 
+// function doSomething() {
+//     let file = document.getElementById('file'),
+//         reader = new FileReader(),
+//         app = document.getElementById('app');
 
 
+//     if(file.files.length){
+//         reader.onload = function(){
+//             let data = JSON.parse(reader.result);
 
+            
+//             console.log(data); // Delete 
 
+//             parseSignup(data,app);
+
+//         };
+//         reader.readAsBinaryString(file.files[0]);
+
+//         // file.parentNode.remove(); 
+//     }
+// }
